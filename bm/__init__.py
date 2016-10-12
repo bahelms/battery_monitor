25b95c128a1e056fefba8f14636bc5128de9ec74
bahelms <jimbonkgit@gmail.com>
2016-10-11 21:13:30 -0400

    fixes config file access error

--- bm/__init__.py
+++ bm/__init__.py
@@ -1,4 +1,7 @@
 import yaml
+import os
 
-with open("config.yml") as f:
+config_file = os.environ.get("CONFIG_FILE") or "config.yml"
+
+with open(config_file) as f:
     config = yaml.load(f.read())
